47425fe0999155afc28734fd37fe9eeb2b3abccf
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-08-21 10:24:03 +1000

    Fixed issue, correctly updating IDPs now

--- SAML_refresh.php
+++ SAML_refresh.php
@@ -3,24 +3,22 @@
 define('CLI_SCRIPT', true);
 require_once('config.php');
 global $DB;
+global $CFG;
+
+use auth_saml2\admin\setting_idpmetadata;
 
 $dbman = $DB->get_manager();
 // Only execute if SAML plugin in installed
 if ($dbman->table_exists('auth_saml2_idps')) {
-    // DELETE DB RECORD FOR CONFIG FILE. MAYBE NOT NEEDED
-    //$DB->delete_records('config_plugins', array('name' => 'idpmetadata', 'plugin' => 'auth_saml2'));
+    //Get the config currently saved in DB
+    $current = get_config('auth_saml2', 'idpmetadata');
 
-    //DELETE DB RECORD FOR CURRENT IDPs
-    $DB->delete_records('auth_saml2_idps');
+    //Construct temp admin control
+    $control = new setting_idpmetadata();
 
-    // PURGE CACHES. MAYBE NOT NEEDED
-    echo shell_exec('php admin/cli/purge_caches.php');
-
-    // RUN CRONJOB TO FORCE IDPREFRESH
-    echo shell_exec("php admin/tool/task/cli/schedule_task.php --execute='\auth_saml2\\task\metadata_refresh'");
+    //Wite setting and force update of IdPs
+    $control->write_setting($current);
 } else {
     echo "SAML2 Plugin not installed\n";
 }
-
-
 
